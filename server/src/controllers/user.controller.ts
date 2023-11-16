7970d45d53f2f29a9ce23d76ecd156559e3e296e
Pawan Maurya <y2k2anomaly@gmail.com>
2023-11-17 02:20:12 +0530

    added update user-profile-picture functionality

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -10,6 +10,7 @@ import sendMail from "../utils/sendMail";
 import { accessTokenOptions, refreshTokenOptions, sendToken } from "../utils/jwt";
 import { redis } from "../utils/redis";
 import { getUserById } from "../services/user.service";
+import cloudinary from "cloudinary";
 
 // register user
 interface IRegistrationBody {
@@ -322,3 +323,56 @@ export const updatePassword = CatchAsyncError(
         }
     }
 );
+
+// update profile picture
+interface IUpdateProfilePicture {
+    avatar: string;
+}
+
+export const updateProfilePicture = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { avatar } = req.body;
+
+        const userId = req.user?._id;
+
+        const user = await userModal.findById(userId);
+
+        if (avatar && user) {
+            // if user have an avatar then call if
+            if (user?.avatar?.public_id) {
+                // first delete the old image
+                await cloudinary.v2.uploader.destroy(user?.avatar?.public_id);
+
+                const myCloud = await cloudinary.v2.uploader.upload(avatar, {
+                    folder: "avatars",
+                    width: 150,
+                });
+                user.avatar = {
+                    public_id: myCloud.public_id,
+                    url: myCloud.secure_url,
+                }
+            } else {
+                const myCloud = await cloudinary.v2.uploader.upload(avatar, {
+                    folder: "avatars",
+                    width: 150,
+                });
+                user.avatar = {
+                    public_id: myCloud.public_id,
+                    url: myCloud.secure_url,
+                }
+            }
+        }
+
+        await user?.save();
+
+        await redis.set(userId, JSON.stringify(user));
+
+        res.status(200).json({
+            success: true,
+            user,
+        })
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+    }
+});
+
